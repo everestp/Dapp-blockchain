83ffd4e506b9413c2dfb992c68d5ce1379cdb5cc
Everest Paudel <paudeleverest09@gmail.com>
2025-04-30 19:50:47 +0545

    refactor: remove old contract and implement new Chai contract with memo functionality

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,47 +1,61 @@
- const hre = require("hardhat")
- async function   main() {
+const {ethers} = require("hardhat")
+async function getBalances(address) {
+  const balanceBigInt = await ethers.provider.getBalance(address);
+  return balanceBigInt;
+}
 
-    const [owner,from1,from2,from3]= await hre.ethers.getSigner();
-    const chai = await hre.ethers.getContractFactory("contract")
-    const  contract = chai.deploy() // instance of contract
+async function cosoleBalances(addresses) {
+  let counter = 0;
+  for (const address of addresses) {
+    console.log(`Address ${counter} balance:`, await getBalances(address));
+    counter++;
+  }
+}
+async function consoleMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+    console.log(
+      `At ${timestamp},name ${name},address ${from},message ${message}`
+    );
+  }
+}
+async function main() {
+  const [owner, from1, from2, from3] = await ethers.getSigners();
+  const chai = await ethers.getContractFactory("chai");
+  const contract = await chai.deploy(); //instance of contract
 
-    await contract.deploy();
-    consoleBalances.log("Address of contract",contract.address);
-    const addresses = [owner.address,from1.address]
-    console.log("Before buying chai")
-    await consoleBalances(addresses)
-    const amount ={value:hre.ethers.ethers.utils.parseEther("1")}
-    await contract.connect(from1).buyChai("from","veryNIce",amount)
-    await contract.connect(from2).buyChai("from2","I want to buld something greate",amount)
-    await contract.connect(from3).buyChai("from3","This is from 2",amount)
+  await contract.connect
+  console.log("Address of contract:", contract.address);
 
- }
-    async function getBalance (address){
-         const balance = await hre.ethers.provider.getBalance(address);
-         return hre.ethers.utils.formatEther(balance)
-    }
+  const addresses = [
+    owner.address,
+    from1.address,
+    from2.address,
+    from3.address,
+  ];
+  console.log("Before buying chai");
+  await cosoleBalances(addresses);
 
-    async function consoleBalances(addresses) {
-        let counter =0;
-        for (const address of addresses){
-            console.log(`Address ${counter} balaces` ,await getBalance(address))
-        }
-    }
-     async function consoleMemo(memos) {
-        for(const memo of memos){
-            const timestamp = memo.timestamp;
-            const name = memo.name;
-            const  from = memo.from;
-            const message = memo.message;
-            console.log(`At ${timestamp}, name ${name} . address ${from} messages ${message}`)
-        }
-        
-     }
-    
- 
+  const amount = { value: ethers.parseEther("10") };
+  await contract.connect(from1).buyChai("from1", "Very nice chai", amount);
+  await contract.connect(from2).buyChai("from2", "Very nice course", amount);
+  await contract
+    .connect(from3)
+    .buyChai("from3", "Very nice information", amount);
 
- main().catch((error) => {
-console.log(error);
-process.exitCode =1 ;
- }
- )
+  console.log("After buying chai");
+  await cosoleBalances(addresses);
+
+  const memos = await contract.getMemos();
+  consoleMemos(memos);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
